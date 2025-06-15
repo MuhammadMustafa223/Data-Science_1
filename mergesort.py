03624f2710eb0982fba36dabdc41e41f84874d47
gullisstillalive <muhammad.mustafa223@amalacademy.org>
2025-06-15 22:39:54 +0200

    modify the grapg code

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,58 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
-
-
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
-
-        mergeSort(left)
-        mergeSort(right)
-
-        l = 0
-        r = 0
-        i = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
-
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
-
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
-
-
+import random
+import time
 import matplotlib.pyplot as plt
+from typing import List
 
-my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+def merge_sort(arr: List[int]) -> List[int]:
+    if len(arr) <= 1:
+        return arr
+
+    middle = len(arr) // 2
+    left = merge_sort(arr[:middle])
+    right = merge_sort(arr[middle:])
+    return merge(left, right)
+
+def merge(left: List[int], right: List[int]) -> List[int]:
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    return merged
+
+def measure_execution_time(arr: List[int]) -> float:
+    start = time.time()
+    merge_sort(arr)
+    end = time.time()
+    return end - start
+
+def plot_performance():
+    input_sizes = [100, 500, 1000, 2000, 5000, 10000]
+    times = []
+
+    for size in input_sizes:
+        test_data = [random.randint(0, 10000) for _ in range(size)]
+        exec_time = measure_execution_time(test_data)
+        times.append(exec_time)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(input_sizes, times, marker='o', linestyle='-', color='blue')
+    plt.title('Merge Sort Performance')
+    plt.xlabel('Input Size (number of elements)')
+    plt.ylabel('Execution Time (seconds)')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("merge_sort_performance.png")
+    plt.show()
+
+if __name__ == "__main__":
+    plot_performance()
+
